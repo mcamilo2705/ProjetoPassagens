852c860693236305a9c7b8ffbb9d84d9aa90ae81
FIC <mcamilo2705@gmail.com>
2025-02-12 21:57:18 -0300

    Inicio do projeto passagem

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿
+string[] nomes = new string[2];
+string[] origens = new string[2];
+string[] destinos = new string[2];
+string[] data = new string[2];
+
+Console.WriteLine("---------------------------");
+Console.WriteLine("Sistema de passagens aereas");
+Console.WriteLine("---------------------------");
+
+EfetuarLogin();
+
+
+Console.WriteLine("Menu Incial");
+Console.WriteLine("Selecione uma opcao: ");
+Console.WriteLine("[1] - Cadastrar passagem "); 
+Console.WriteLine("[2] - Listar as passagens "); 
+Console.WriteLine("[0] - Sair do Sistema "); 
+
+int opcao = int.Parse(Console.ReadLine());
+
+switch (opcao)
+{
+   
+}
+
+
+
+static void EfetuarLogin()
+{
+
+    string senhaRecebida;
+
+    //faca enquanto
+    do
+    {
+        Console.Write("Digite a senha: ");
+        senhaRecebida = Console.ReadLine();
+    } while (senhaRecebida != "123456");
+
+
+}
+
